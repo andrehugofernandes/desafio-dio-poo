fe926c15dd1a640926f912cc37653071b88d29ee
andrehugofernandes <andrehugofernandes@gmail.com>
2023-01-06 00:09:23 -0300

    Created calss Conteudo - Herança e Polimorfismo parte 1.

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+import br.com.dio.desafio.dominio.Conteudo;
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso de Java");
+        curso1.setDescricao("Descrição do curso de Java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso de Python");
+        curso2.setDescricao("Descrição do curso de Python");
+        curso2.setCargaHoraria(4);
+
+        Conteudo conteudo = new Curso();
+        Conteudo conteudo1 = new Mentoria();
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria de Java");
+        mentoria.setDescricao("Descrição da mentoria e Java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso2);
+        System.out.println(curso1);
+        System.out.println(mentoria);
+
+
+
+
+
+    }
+}
